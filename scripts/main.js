0991be095552d311f95dfcc454f30e75dabf3803
wesley-hall <hallwesley@outlook.com>
2019-02-17 09:56:17 +0000

    gameboard added, createBoard function working in js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,45 @@
-console.log('it works')
+
+$(() => {
+
+  console.log('it works')
+
+  const pacman = {
+    currentPosition: 0
+  }
+
+  const $gameboard = $('.gameboard')
+
+  const tiles = 400
+  function createBoard() {
+    for (let i = 0; i < tiles; i++) {
+      $gameboard.append(`<div id="${i}" class="tile"></div>`)
+    }
+  }
+
+  createBoard()
+
+})
+
+
+// Move
+// Remove class
+// Add class elsewhere
+
+// Move left
+// current position - 1
+
+// Move up
+// current position - 20
+
+// Move right
+// current position + 1
+
+// Move down
+// current position + 20
+
+
+// keyCode for arrows
+// left 37
+// up 38
+// right 39
+// down 40

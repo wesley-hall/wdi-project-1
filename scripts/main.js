20b0617456a58e81e7d2d33f296a75a43386cec3
wesley-hall <hallwesley@outlook.com>
2019-02-17 10:07:59 +0000

    placed pacman using pacman obj current position

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,11 @@ $(() => {
 
   console.log('it works')
 
-  const $gameboard = $('.gameboard')
 
+  const $gameboard = $('.gameboard')
   const tiles = 400
+
+  // Create the game board with div.tile children
   function createBoard() {
     for (let i = 0; i < tiles; i++) {
       $gameboard.append(`<div id="${i}" class="tile"></div>`)
@@ -13,6 +15,29 @@ $(() => {
   }
 
   createBoard()
+
+
+  const $tiles = $('.tile')
+  console.log($tiles)
+
+  const pacman = {
+    currentPosition: 42
+  }
+
+
+  function placePacman() {
+    $tiles.eq(pacman.currentPosition).addClass('pacman')
+  }
+
+  placePacman()
+
+
+
+
+
+
+
+
 
 })
 

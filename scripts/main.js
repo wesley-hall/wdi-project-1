ac41844cbdda07f86a12b1f8d9c91591fe6d6266
wesley-hall <hallwesley@outlook.com>
2019-02-17 14:36:15 +0000

    pacman images, css ghost colours, js directions, js ghost movement random

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,11 @@ $(() => {
 
 
   const $gameboard = $('.gameboard')
-  const tiles = 400
+  const numOfTiles = 400
 
   // Create the game board with div.tile children
   function createBoard() {
-    for (let i = 0; i < tiles; i++) {
+    for (let i = 0; i < numOfTiles; i++) {
       $gameboard.append(`<div id="${i}" class="tile"></div>`)
     }
   }
@@ -20,10 +20,20 @@ $(() => {
   const $tiles = $('.tile')
   console.log($tiles)
 
-  const pacman = {
-    currentPosition: 42
+
+  class Character {
+    constructor(currentPosition) {
+      this.currentPosition = currentPosition
+    }
+    movement() {
+
+    }
   }
 
+
+  const pacman = new Character(42)
+
+  // const $pacman = $('.pacman')
 
   function placePacman() {
     $tiles.removeClass('pacman')
@@ -32,19 +42,76 @@ $(() => {
 
   placePacman()
 
+  // Order is left, up, right, down)
+  const directions = {
+    left: -1,
+    up: -20,
+    right: 1,
+    down: 20
+  }
+
+  const directionOptions = [directions.left, directions.up, directions.right, directions.down]
+
 
   function movePacman() {
     $('body').on('keydown', function(e) {
       if (e.which === 37) {
-        pacman.currentPosition--
-        console.log(pacman.currentPosition)
+        pacman.currentPosition+= directions.left
         placePacman()
       }
+      if (e.which === 38) {
+        pacman.currentPosition+= directions.up
+        placePacman()
+      }
+      if (e.which === 39) {
+        pacman.currentPosition+= directions.right
+        placePacman()
+      }
+      if (e.which === 40) {
+        pacman.currentPosition+= directions.down
+        placePacman()
+      }
+      console.log(pacman.currentPosition)
     })
   }
 
   movePacman()
 
+  class Ghost extends Character {
+    constructor(currentPosition) {
+      super(currentPosition)
+    }
+  }
+
+  const blinky = new Ghost(189)
+  const pinky = new Ghost(190)
+  const inky = new Ghost(209)
+  const clyde = new Ghost(210)
+
+
+  function placeGhosts() {
+    $tiles.removeClass('ghost blinky pinky inky clyde')
+    $tiles.eq(blinky.currentPosition).addClass('ghost blinky')
+    $tiles.eq(pinky.currentPosition).addClass('ghost pinky')
+    $tiles.eq(inky.currentPosition).addClass('ghost inky')
+    $tiles.eq(clyde.currentPosition).addClass('ghost clyde')
+  }
+
+  placeGhosts()
+
+  const ghostInterval = setInterval(moveGhosts, 1000)
+
+  function moveGhosts(ghost) {
+    const ghostDirection = directionOptions[Math.floor(Math.random() * directionOptions.length)]
+    console.log(ghostDirection)
+    ghost.currentPosition+= ghostDirection
+    placeGhosts()
+  }
+
+  moveGhosts(blinky)
+
+  const $stopGhosts = $('#stop')
+  $stopGhosts.on('click', clearInterval(ghostInterval))
 
 
 

18deef3dd7e97e061cc850e6516fcdeab49afb8f
wesley-hall <hallwesley@outlook.com>
2019-02-17 09:57:41 +0000

    js comments for movement and keyCodes

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,26 @@ $(() => {
   createBoard()
 
 })
+
+// Move
+// Remove class
+// Add class elsewhere
+
+// Move left
+// current position - 1
+
+// Move up
+// current position - 20
+
+// Move right
+// current position + 1
+
+// Move down
+// current position + 20
+
+
+// keyCode for arrows
+// left 37
+// up 38
+// right 39
+// down 40

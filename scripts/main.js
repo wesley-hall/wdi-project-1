39a0e7fce4af5640b841713f70d9be8001300396
wesley-hall <hallwesley@outlook.com>
2019-02-20 11:27:06 +0000

    removed some planning comments

--- scripts/main.js
+++ scripts/main.js
@@ -112,9 +112,7 @@ $(() => {
   }
 
 
-  // let previousPosition
-
-  // Characters - Pac-Man
+    // Characters - Pac-Man
   class Pacman extends Character {
     constructor(startPosition) {
       super(startPosition)
@@ -298,26 +296,3 @@ $(() => {
 
 
 })
-
-// Move
-// Remove class
-// Add class elsewhere
-
-// Move left
-// current position - 1
-
-// Move up
-// current position - 20
-
-// Move right
-// current position + 1
-
-// Move down
-// current position + 20
-
-
-// keyCode for arrows
-// left 37
-// up 38
-// right 39
-// down 40
